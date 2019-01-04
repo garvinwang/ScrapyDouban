11c68e4fef5c8f82c09dd6a15d1ac519d0daaf1e
wanghai <wh@qq.com>
2019-01-04 11:19:59 +0800

    激活代理

--- scrapy/douban/settings.py
+++ scrapy/douban/settings.py
@@ -29,7 +29,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 CONCURRENT_REQUESTS_PER_IP = 1
@@ -54,9 +54,9 @@ COOKIES_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#     'douban.middlewares.ProxyMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    'douban.middlewares.ProxyMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
